c43fc77b2d6f4b47dca8c27d67870ed8d5219bf3
Lena Viktorsson <lena.viktorsson@smhi.se>
2025-08-18 16:05:57 +0200

    update dataframe correctly
    - adds row_id before each qc check to keep track of changed rows
    - added test to run qc pipeline on randomised larger dataset

--- src/ocean_data_qc/fyskem/base_qc_category.py
+++ src/ocean_data_qc/fyskem/base_qc_category.py
@@ -9,6 +9,9 @@ from ocean_data_qc.fyskem.qc_flags import QcFlags
 class BaseQcCategory(abc.ABC):
     def __init__(self, data: pl.DataFrame, field_position: int, column_name: str):
         self._data = data
+        # Ensure we have a stable row identifier
+        if "_row_id" not in data.columns:
+            self._data = data.with_columns(pl.arange(0, data.height).alias("_row_id"))
         self._field_position = field_position
         self._column_name = column_name
         self._info_column_name = f"info_{column_name}"
@@ -42,8 +45,6 @@ class BaseQcCategory(abc.ABC):
                 pl.lit(str(QcFlag.NO_QC_PERFORMED)).alias(self._info_column_name),
             ]
         )
-        # self._data[self._column_name] = str(QcFlag.NO_QC_PERFORMED.value)
-        # self._data[self._info_column_name] = str(QcFlag.NO_QC_PERFORMED)
 
     def collapse_qc_columns(self):
         # Insert the specific QC flag into the correct position in AUTO_QC
@@ -72,6 +73,7 @@ class BaseQcCategory(abc.ABC):
         )
 
     def update_dataframe(self, selection, result_expr):
+        # Add the QC results to the selection
         selection = (
             selection.with_columns([result_expr.alias("result_struct")])
             .with_columns(
@@ -84,17 +86,22 @@ class BaseQcCategory(abc.ABC):
             )
             .drop("result_struct")
         )
-        # Update original dataframe with qc results
-        parameter_boolean = pl.col("parameter") == self._parameter
-        self._data = self._data.with_columns(
-            [
-                pl.when(parameter_boolean)
-                .then(selection[self._column_name])
-                .otherwise(pl.col(self._column_name))
-                .alias(self._column_name),
-                pl.when(parameter_boolean)
-                .then(selection[self._info_column_name])
-                .otherwise(pl.col(self._info_column_name))
-                .alias(self._info_column_name),
-            ]
+
+        # Collect that data that should be updated in the original dataframe
+        # only the two update_cols should be updated
+        # (total flag is updated after all checks)
+        update_cols = [self._column_name, self._info_column_name]
+        update_df = selection.select(["_row_id", *update_cols])
+
+        self._data = self._data.join(
+            update_df, on="_row_id", how="left", suffix="_update"
         )
+
+        # Replace columns only where new data exists
+        for col in update_cols:
+            self._data = self._data.with_columns(
+                pl.when(pl.col(f"{col}_update").is_not_null())  # update from qc exists
+                .then(pl.col(f"{col}_update"))
+                .otherwise(pl.col(col))
+                .alias(col)
+            ).drop(f"{col}_update")
